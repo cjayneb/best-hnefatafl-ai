0c425b748e567c0b820592172e0dbf204ce935e2
Chomosuke123 <122450828+Chomosuke123@users.noreply.github.com>
2023-10-29 17:15:29 -0400

    GetMoves + deleteMove + addMove + AfficheBoard

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Client {
     public static void main(String[] args) {
@@ -8,6 +12,20 @@ class Client {
         BufferedInputStream input;
         BufferedOutputStream output;
         int[][] board = new int[13][13];
+        Map<Character, Integer> letterToY = new HashMap<>();
+        letterToY.put('A', 0);
+        letterToY.put('B', 1);
+        letterToY.put('C', 2);
+        letterToY.put('D', 3);
+        letterToY.put('E', 4);
+        letterToY.put('F', 5);
+        letterToY.put('G', 6);
+        letterToY.put('H', 7);
+        letterToY.put('I', 8);
+        letterToY.put('J', 9);
+        letterToY.put('K', 10);
+        letterToY.put('L', 11);
+        letterToY.put('M', 12);
 
         try {
             MyClient = new Socket("localhost", 8888);
@@ -28,7 +46,7 @@ class Client {
                     //System.out.println("size " + size);
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer).trim();
-                    System.out.println(s);
+                    //System.out.println(s);
                     String[] boardValues;
                     boardValues = s.split(" ");
                     int x=0,y=0;
@@ -40,15 +58,25 @@ class Client {
                             y++;
                         }
                     }
-
+                    AfficherBoard(board);
                     System.out.println("Nouvelle partie! Vous jouer blanc, entrez votre premier coup : ");
+                    
+                    //Voir tous les coups possibles de E1
+                    List<int[]> possibleMovesE1 = new ArrayList<>();
+                    possibleMovesE1 = getPossibleMoves(board, 12, 2);
+                    System.out.println("Possible moves for roi: ");
+                    for (int[] move : possibleMovesE1) {
+                        System.out.println(move[0] + " " + move[1]);
+                    }
+
                     String move = null;
                     move = console.readLine();
+                    updateBoard(move, board, letterToY);
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
                 }
-                // Debut de la partie en joueur Noir
-                if(cmd == '2'){
+                // Debut de la partie en joueur Noir 
+                if(cmd == '2'){ 
                     System.out.println("Nouvelle partie! Vous jouer noir, attendez le coup des blancs");
                     byte[] aBuffer = new byte[1024];
 
@@ -63,7 +91,7 @@ class Client {
                     for(int i=0; i<boardValues.length;i++){
                         board[x][y] = Integer.parseInt(boardValues[i]);
                         x++;
-                        if(x == 8){
+                        if(x == 13){
                             x = 0;
                             y++;
                         }
@@ -82,9 +110,12 @@ class Client {
 
                     String s = new String(aBuffer);
                     System.out.println("Dernier coup :"+ s);
+                    updateBoard(s, board, letterToY);
+                    AfficherBoard(board);
                     System.out.println("Entrez votre coup : ");
                     String move = null;
                     move = console.readLine();
+                    updateBoard(move, board, letterToY);
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
 
@@ -118,5 +149,89 @@ class Client {
         }
 
     }
+
+    public static List<int[]> getPossibleMoves(int[][] board, int x, int y) {
+        List<int[]> possibleMoves = new ArrayList<>();
+        boolean roi = false;
+
+        if(board[x][y] == 5){
+            roi = true;
+        }
+        
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; 
+        
+        for (int[] direction : directions) {
+            int dx = direction[0];
+            int dy = direction[1];
+            int newX = x + dx;
+            int newY = y + dy;
+            
+            //ATTENTION IL RESTE ENCORE A GERER LA CASE DU MILIEU
+            if(roi){ //Parce que le roi peut aller dans les coins 
+                while (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && board[newX][newY] == 0 ) {
+                possibleMoves.add(new int[]{newX, newY});
+                newX += dx;
+                newY += dy;
+            }
+            }else{
+                while (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && board[newX][newY] == 0 && !estCoin(newX, newY)) {
+                possibleMoves.add(new int[]{newX, newY});
+                newX += dx;
+                newY += dy;
+                }
+            }
+            
+        }
+        
+        return possibleMoves;
+    }
+
+    public static void updateBoard(String dernierMove, int[][] board, Map<Character, Integer> letterToY) {
+        String [] dernierMoveTab = dernierMove.split("-");
+        int joueur = deleteMove(dernierMoveTab[0], board, letterToY);
+        addMove(dernierMoveTab[1], board, joueur, letterToY);
+    }
+
+    public static int deleteMove(String s, int[][] board, Map<Character, Integer> letterToY){
+        int x = 13 - Integer.parseInt(s.trim().substring(1)); //Soustraction car le board est inversé
+        int y = letterToY.get(s.trim().charAt(0));
+        int joueur = board[x][y];
+        board[x][y] = 0;
+        return joueur;
+    }
+
+    public static void addMove(String s, int[][] board, int joueur, Map<Character, Integer> letterToY){
+        int x = 13 - Integer.parseInt(s.trim().substring(1)); //Soustraction car le board est inversé
+        int y = letterToY.get(s.trim().charAt(0));
+        board[x][y] = joueur;
+    }
+
+    public static void AfficherBoard(int[][] board){
+        System.out.println("BOARD : ");
+        //print the board 
+        for(int i=0; i<13;i++){
+            for(int j=0; j<13;j++){ 
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean estCoin(int x, int y){
+        if(x == 0 && y == 0){
+            return true;
+        }
+        if(x == 0 && y == 12){
+            return true;
+        }
+        if(x == 12 && y == 0){
+            return true;
+        }
+        if(x == 12 && y == 12){
+            return true;
+        }
+        return false;
+    }
+
 }
 
